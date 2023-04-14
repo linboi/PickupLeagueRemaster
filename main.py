3ca50b60e0667fa7c19cb85ca07c4ac3d066168c
linboi <tedblonde@gmail.com>
2023-04-15 00:20:27 +0100

    tab consistency

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+	print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+	if message.author == client.user:
+		return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+	if message.content.startswith('$hello'):
+		await message.channel.send('Hello!')
 
 def main():
 	with open('./secret.json') as f:
 	
 		secret = json.load(f)
-                
+				
 	with open('./settings.json') as f:
 	
 		settings = json.load(f)
 
 	client.run(secret['BOT_TOKEN'])
-        
+		
 if __name__ == '__main__':
 	main()
