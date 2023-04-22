f41a43dbdcb02252961c43d224069c47fc3ffac8
Cailean Finn <Caileannn@gmail.com>
2023-04-22 23:33:39 +0100

    comment

--- serverInstance.py
+++ serverInstance.py
@@ -96,8 +96,6 @@ class serverInstance:
 
 		print(f"Game Count: {len(self.currentMatches)}")
 		
-
-	
 	
 	# Scrape rank details from op.gg page
 	async def signUpPlayer(self, msg_content, message_obj):
@@ -376,19 +374,4 @@ class serverInstance:
 			else:
 				return False
 
-	# Fetch player detials -> returns a player object
-	async def createPlayerObject(self, playerID):
-		
-		# Fetch player details
-		res = self.cursor.execute(f"SELECT * FROM Player WHERE playerID = {playerID}")
-		player_details = res.fetchone()
-  
-		# Fetch account details
-		res = self.cursor.execute(f"SELECT * FROM Account WHERE playerID = {playerID}")
-		player_acc_detials = res.fetchall()
-		
-		# Create player
-		player = Player(player_details[0], player_details[1], player_details[2], player_details[3], player_details[4], player_details[5], player_details[6], player_acc_detials, self.cursor, self.con)
-		
-		return player
   
