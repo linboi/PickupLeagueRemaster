706b859895b0548abb4d3f85307eeac57e4ad12a
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2023-06-02 14:37:51 +0100

    Cleanup team.py (#93)
    * Format files with autopep
    
    * Cleanup team.py
    
    * fix merge bug
    
    ---------
    
    Co-authored-by: linboi <tedblonde@gmail.com>

--- team.py
+++ team.py
@@ -1,28 +1,10 @@
 class Team:
-    def __init__(self):
-
-        self.top = None
-        self.jungle = None
-        self.mid = None
-        self.adc = None
-        self.support = None
-        self.avgMMR = 0
-
     def __init__(self, top, jungle, mid, adc, support):
-
-        top.set_role("TOP")
-        self.top = top
-        jungle.set_role("JNG")
-        self.jungle = jungle
-        mid.set_role("MID")
-        self.mid = mid
-        adc.set_role("ADC")
-        self.adc = adc
-        support.set_role("SUP")
-        self.support = support
-        # for player in self.getListPlayers():
-        # player.set_role
-        # self.calculateAvgMMR()
+        self.set_top(top)
+        self.set_jg(jungle)
+        self.set_mid(mid)
+        self.set_adc(adc)
+        self.set_sup(support)
 
     # Setters
     def set_avgMMR(self, MMR):
@@ -32,13 +14,13 @@ class Team:
         self.top = player
         self.top.set_role("top")
 
-    def set_mid(self, player):
-        self.mid = player
-        self.mid.set_role("mid")
-
     def set_jg(self, player):
         self.jungle = player
         self.jungle.set_role("jng")
+
+    def set_mid(self, player):
+        self.mid = player
+        self.mid.set_role("mid")
 
     def set_adc(self, player):
         self.adc = player
@@ -49,8 +31,8 @@ class Team:
         self.support.set_role("sup")
 
     # Getters
-
     def get_avgMMR(self):
+        self.calculate_avg_mmr()
         return self.avgMMR
 
     def get_top(self):
@@ -68,32 +50,26 @@ class Team:
     def get_sup(self):
         return self.support
 
-    # Sets AVGMMR of the Team
-    def calculateAvgMMR(self):
-        playerList = self.getListPlayers()
-        mmr_sum = 0
-        for player in playerList:
-            pMMR = player.get_roleMMR()
-            mmr_sum += pMMR
+    def calculate_avg_mmr(self):
+        total_mmr = 0
+        player_list = self.getListPlayers()
+        for player in player_list:
+            total_mmr += player.get_roleMMR()
+        self.set_avgMMR(total_mmr / 5)
 
-        self.set_avgMMR(mmr_sum / 5)
+    def get_player_list(self):
+        players = [self.top, self.jungle,
+                   self.mid, self.adc, self.support]
+        return players
 
-    # List of players -> returns array of player objects
-    def getListPlayers(self):
-        listOfPlayers = [self.top, self.jungle,
-                         self.mid, self.adc, self.support]
-        return listOfPlayers
-
-    # List MULTI OPGG
-    def listOPGG(self):
+    def get_multi_opgg(self):
         players = self.getListPlayers()
-        player_string = "https://www.op.gg/multisearch/euw?summoners="
-        ign = ""
+        multi_opgg = "https://www.op.gg/multisearch/euw?summoners="
         for player in players:
-            ign = player.getHighestAccountName()
-            if ign == None:
-                ign = "placeholder"
-            ign = ign.replace(" ", "+")
-            player_string += f"{ign}%2C+"
-
-        return player_string
+            username = player.getHighestAccountName()
+            if username != None:
+                username = username.replace(" ", "+")
+                multi_opgg += f"{username},"
+            else:
+                print(f"No opgg found for  {player}")
+        return multi_opgg
