9ebc25b0c9bbb6653d51cd1b310325cd489bd469
Cailean Finn <Caileannn@gmail.com>
2023-06-03 00:16:25 +0100

    rasp update (#106)

--- rasp.py
+++ rasp.py
@@ -6,11 +6,11 @@ class rasp:
     def restart_pi():
         # restart pi
         process = subprocess.Popen(
-            ["sudo", "reboot"], shell=True, stdout=subprocess.PIPE)
+            ["sudo", "reboot"], stdout=subprocess.PIPE)
 
     def update_pi():
         # update bot version
         process = subprocess.Popen(
-            ["git", "pull", "origin", "release"], shell=True, stdout=subprocess.PIPE)
+            ["git", "pull", "origin", "release"], stdout=subprocess.PIPE)
         output = process.communicate()[0]
         return output
