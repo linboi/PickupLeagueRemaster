307d83ee32fc8e342b82f03851b52d3df1bf67d3
Cailean Finn <Caileannn@gmail.com>
2023-06-02 19:30:30 +0100

    remove print output

--- rasp.py
+++ rasp.py
@@ -7,13 +7,10 @@ class rasp:
         # restart pi
         process = subprocess.Popen(
             ["sudo", "reboot"], shell=True, stdout=subprocess.PIPE)
-        output = process.communicate()[0]
-        print(output)
 
     def update_pi():
         # update bot version
         process = subprocess.Popen(
             ["git", "pull", "origin", "release"], shell=True, stdout=subprocess.PIPE)
         output = process.communicate()[0]
-        print(output)
         return output
